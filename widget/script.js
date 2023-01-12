2a05f05f82c4e417fb86abee06a59ace424a235e
phuonghoangcabin <phuonghoang@cabinfood.vn>
2023-01-13 05:00:43 +0700

    widget

---
+++ widget/script.js
@@ -0,0 +1,10 @@
+$("#launch").click(function () {
+  $("#smith-container").toggle("fast");
+  $("#launch").toggleClass("active");
+  $("body").toggleClass("noscroll");
+});
+$("#close").click(function () {
+  $("#smith-container").toggle("fast");
+  $("#launch").toggleClass("active");
+  $("body").toggleClass("noscroll");
+});
